2fc2152363f87e3e9a3b5994873be7bf02119912
Vidhya2k <padmashrividhya@gmail.com>
2018-12-24 16:40:39 -0600

    Add project files.

---
+++ TaskManagerUI/demo.js
@@ -0,0 +1,109 @@
+﻿
+var MyApp = angular.module("MyApp", ['ngRoute', 'TaskManagerService']);
+
+MyApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/Add', {
+                templateUrl: 'Views/add.html',
+                controller: 'AddController'
+            }).
+            when('/Edit', {
+                templateUrl: 'Views/edit.html',
+                controller: 'EditController'
+            }).
+            when('/Delete', {
+                templateUrl: 'Views/delete.html',
+                controller: 'DeleteController'
+            }).
+            when('/Home', {
+                templateUrl: 'Views/home.html',
+                controller: 'HomeController'
+            }).
+            otherwise({
+                redirectTo: '/Home'
+            });
+    }]);
+
+MyApp.controller("AddController", function ($scope, TaskApi) {
+
+    $scope.addTask = function () {
+        var taskToAdd = {
+            'Task_Description': $scope.Task_Description,
+            'Priority': $scope.Priority,
+            'Parent_ID': $scope.Parent_ID,
+            'Start_Date': $scope.Start_Date,
+            'End_Date': $scope.End_Date
+        };
+        TaskApi.addTask(taskToAdd)
+            .then(function (response) {
+                alert("Task added successfully!!");
+                $scope.Task_Description = undefined;
+                $scope.Priority = undefined;
+                $scope.Parent_ID = undefined;
+                $scope.Start_Date = undefined;
+                $scope.End_Date = undefined;
+            })
+    }
+});
+
+   
+MyApp.controller("EditController", function ($scope) {
+
+    $scope.message = "In Edit"
+
+});
+
+MyApp.controller("DeleteController", function ($scope) {
+
+    $scope.message = "In Delete"
+
+});
+
+
+
+MyApp.controller("HomeController", function ($scope, TaskApi) {
+
+    getTasks();
+
+    function getTasks() {
+        TaskApi.getTasks().then(function (tasks) {
+            $scope.tasks = tasks.data;
+        })
+    }
+
+    $scope.updateEndDate = function (task) {
+        var taskToEdit = {
+            'Task_Description': task.Task_Description,
+            'Priority': task.Priority,
+            'Parent_ID': task.Parent_ID,
+            'Start_Date': task.Start_Date,
+            'End_Date': new Date(),
+            'Task_ID': task.Task_ID
+        };
+
+        TaskApi.editTask(taskToEdit).then(function (response) {
+            alert('Task ended successfully!')
+        });
+    }
+
+    $scope.editTask = function (task) {
+        alert("esawdwefd")
+    }
+
+
+    //$scope.editTask = function (task) {
+    //    var taskToEdit = {
+    //        'Task_Description': task.Task_Description,
+    //        'Priority': task.Priority,
+    //        'Parent_ID': task.Parent_ID,
+    //        'Start_Date': task.Start_Date,
+    //        'End_Date': task.End_Date,
+    //        'Task_ID': task.Task_ID
+    //    };
+
+    //    TaskApi.editTask(taskToEdit).then(function (response) {
+    //        alert('Task ended successfully!')
+    //    });
+    //}
+});
---
+++ TaskManagerUI/service.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="scripts/angular.js" />
+
+var TaskManagerService = angular.module('TaskManagerService', []);
+
+TaskManagerService.factory('TaskApi', function ($http) {
+
+    var urlBase = "http://localhost:57071/api";
+    var TaskApi = {};
+
+
+    TaskApi.getTasks = function()
+    {
+        var url = urlBase + '/tasks';
+        var result = $http.get(url);
+        return result;
+    };
+
+    TaskApi.addTask = function (task) {
+        var url = urlBase + '/tasks';
+        return $http.post(url, task);        
+    };
+
+    TaskApi.editTask = function (task) {
+        var url = urlBase + '/tasks/' + task.Task_ID;
+        return $http.put(url, task);
+    };
+
+    return TaskApi;
+});
